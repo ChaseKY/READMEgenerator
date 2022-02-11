2cfd21c3d81456fcc20661148d3747e56bf5f59a
Chase Young <chasekyoung@gmail.com>
2022-02-11 16:13:56 -0500

    added some questions and got them working in terminal

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
+// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown')
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+    type: 'input',
+    name: 'title',
+    message: 'What is the name of your project?'
+},  {
+    type: 'input',
+    name: 'description',
+    message: 'Write a description of your project.'
+}
 
-const generateREADME = ({ title, description, })
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        console.log(answers);
+        generateMarkdown(answers);
+
+    })
+}
+
+// Function call to initialize app
+init();
